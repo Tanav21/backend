c098f3130bab7e8f396252199e04730622797687
Tanav <tanavmahendru2003@gmail.com>
2026-01-16 20:10:36 +0530

    cors

--- index.js
+++ index.js
@@ -20,12 +20,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
